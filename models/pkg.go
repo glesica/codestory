ba15a827158138bfed1b28201f37e9d9cf9ccc2d
George Lesica <george@lesica.com>
2018-12-17 22:12:20 -0700

    Use maps for funs and structs

--- models/pkg.go
+++ models/pkg.go
@@ -6,12 +6,20 @@ import (
 )
 
 type Pkg struct {
-	Funs    []*Fun    `json:"funs"`
-	Name    string    `json:"name"`
-	Structs []*Struct `json:"structs"`
+	Funs    map[string]*Fun    `json:"funs"`
+	Name    string             `json:"name"`
+	Structs map[string]*Struct `json:"structs"`
 }
 
 func (p *Pkg) processFunction(decl *ast.FuncDecl, fileName string, fileSet *token.FileSet) {
+	if p.Funs == nil {
+		p.Funs = make(map[string]*Fun)
+	}
+
+	if p.Structs == nil {
+		p.Structs = make(map[string]*Struct)
+	}
+
 	fun := newFromAST(decl, fileName, fileSet)
-	p.Funs = append(p.Funs, fun)
+	p.Funs[fun.Name] = fun
 }
